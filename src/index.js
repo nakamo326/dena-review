666e0e93c0676d5ad41da700c379fcef6fdca9a5
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-12-07 01:37:15 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -139,16 +139,18 @@ class Game extends React.Component {
 
     return (
       <div className="game">
-        <div className="game-title"></div>
-        <div className="game-board">
-          <Board 
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+        <div className="game-title"> 電脳盤上娯楽 四子棋 </div>
+        <div className="game-body">
+          <div className="game-board">
+            <Board 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
         </div>
       </div>
     );
