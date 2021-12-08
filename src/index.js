e25319f0bf6944e7cae79232631a69cfe73e3523
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-12-08 17:08:27 +0900

    add mobile.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './mobile.css';
 
 import Board from './components/board';
 import Volume from './components/volume';
