598bc9ee4dab7eeff675f397f8e69b38a87d4756
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-12-10 13:10:12 +0900

    fix utils

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,21 +1,15 @@
 //  return placeable index number, or return null if stone is full on col
-export function isPlaceable(squares, i) {
-  const col = i % 7;
-  for (let row = 0; row < 6; row++) {
+export function isPlaceable(squares, col) {
+  for (let row = 5; row >= 0; row--) {
     const index = row * 7 + col;
-    if (squares[index] !== null) {
-      if (row === 0) return null;
-      else return index - 7;
-    }
+    if (squares[index] === null) return index;
   }
-  return col + 35;
+  return null;
 }
 
 export function calculateWinner(squares, index) {
-  if (index === 42) {
-    return null;
-  }
-  if (squares[index] === null) return calculateWinner(squares, index + 1);
+  const row = Math.trunc(index / 7);
+  const col = index % 7;
   const dirList = [
     [0, 1],
     [-1, 1],
@@ -27,29 +21,27 @@ export function calculateWinner(squares, index) {
     [1, 1],
   ];
 
-  const row = Math.trunc(index / 7);
-  const col = index % 7;
-
-  for (let i = 0; i < 8; i++) {
-    const [a, b, c, d] = [
-      (row + dirList[i][0] * 0) * 7 + (col + dirList[i][1] * 0),
-      (row + dirList[i][0] * 1) * 7 + (col + dirList[i][1] * 1),
-      (row + dirList[i][0] * 2) * 7 + (col + dirList[i][1] * 2),
-      (row + dirList[i][0] * 3) * 7 + (col + dirList[i][1] * 3),
-    ];
-    if (!checkJump(i, [a, b, c])) continue;
-    if (squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]) {
-      console.log(a);
-      console.log(b);
-      console.log(c);
-      console.log(d);
-      return [a, b, c, d];
+  if (index === 42) {
+    return null;
+  }
+  if (squares[index] !== null) {
+    for (let i = 0; i < 8; i++) {
+      const [a, b, c, d] = [
+        (row + dirList[i][0] * 0) * 7 + (col + dirList[i][1] * 0),
+        (row + dirList[i][0] * 1) * 7 + (col + dirList[i][1] * 1),
+        (row + dirList[i][0] * 2) * 7 + (col + dirList[i][1] * 2),
+        (row + dirList[i][0] * 3) * 7 + (col + dirList[i][1] * 3),
+      ];
+      if (!isOverRun(i, [a, b, c])) continue;
+      if (squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]) {
+        return [a, b, c, d];
+      }
     }
   }
   return calculateWinner(squares, index + 1);
 }
 
-function checkJump(dir, array) {
+function isOverRun(dir, array) {
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
     const row = Math.trunc(element / 7);
