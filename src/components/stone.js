68f18c6a2bfe39030d46a74eed4c38196b0a6960
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-12-10 11:05:46 +0900

    fix stone

--- src/components/stone.js
+++ src/components/stone.js
@@ -1,15 +1,6 @@
-export function Stone(props) {
+export default function Stone(props) {
   let color;
   if (props.value === null) color = '';
   else color = props.value === 'X' ? 'stone-color1' : 'stone-color2';
-  return (
-    <button
-      className={'stone ' + color}
-      onClick={() => {
-        props.onClick();
-      }}
-    />
-  );
+  return <button className={'stone ' + color} onClick={props.onClick} />;
 }
-
-export default Stone;
